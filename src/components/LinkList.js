b68a260b7d45c0e7844820640c139b19b32c5bbf
Gilbert <gssenyonjo@gmail.com>
2019-12-24 23:56:22 +0300

    used  graphql to populate LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import Link from './Link'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
 
+
+const LINK_QUERY = gql`
+    {
+        links {
+            id
+            url
+        }
+    }
+
+`
 export default function LinkList(props) {
-
-    return (
-        <div>
-            {links.map(
-                link => <Link 
-                    key={link.id}
-                    link={link}
-                    />
-            )}
-        </div>
+    
+    return (        
+        <Query query={LINK_QUERY}>
+            {({ loading, error, data }) => {
+                if(loading) return <div>Fetching</div>
+                if(error) return <div>Error</div>
+                const links = data.links                
+                return (
+                    links.map(
+                    link => <Link 
+                        key={link.id}
+                        link={link}
+                        />
+                    )
+                )
+            }}
+        </Query>
     )
 }
 
-const links = [
+const localLinks = [
     {
         id: 1,
         url: "https://prisma.com"
